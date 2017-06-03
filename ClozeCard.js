1ff5a0b0796fe37519218e03568c80bb630ff7b4
nhuyp <ypham87@gmail.com>
2017-06-02 18:31:13 -0700

    ClozeCard cloze and fulltext arguments, began working on partial

---
+++ ClozeCard.js
@@ -0,0 +1,26 @@
+var fullText = "George Washington was the first president of the United States.";
+
+var cloze = "George Washington";
+
+// var clozeArray = cloze.split(',');
+// console.log(clozeArray);
+// var textArray = fullText.split(',');
+// console.log(textArray); 
+
+// var partial = textArray.filter(function(val) {
+// return (clozeArray.indexOf(val) == -1);
+// }); 
+
+// console.log (textArray); 
+
+var textArray = fullText.substring(); 
+console.log(textArray); 
+
+var clozeArray = cloze.substring();
+console.log(clozeArray);
+
+var partial = function () {
+    for (var i = 0; i < textArray; i++) {
+        if 
+    }
+}
